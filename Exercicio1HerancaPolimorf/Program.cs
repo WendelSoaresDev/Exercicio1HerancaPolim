5777f4145377563c7cf713ecba8816871eeb1d3c
Wendel Soares <wendel.soares.dev@gmail.com>
2022-02-22 00:27:30 -0300

    Exercício finalizado

--- Exercicio1HerancaPolimorf/Program.cs
+++ Exercicio1HerancaPolimorf/Program.cs
@@ -1,9 +1,53 @@
-﻿namespace Exercicio1HerancaPolimorf
+﻿using System;
+using Exercicio1HerancaPolimorf.Entities;
+using System.Globalization;
+
+namespace Exercicio1HerancaPolimorf
 {
     class Program
     {
         public static void Main(String[] args)
         {
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Outsourced (y/n):");
+                char outEmp = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valueHour = double.Parse(Console.ReadLine());
+
+                if (outEmp == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double addCharge = double.Parse(Console.ReadLine());
+                    list.Add(new OutsourceEmployee(name, hours, valueHour, addCharge));
+
+                }
+
+                else 
+                {
+                    list.Add(new Employee(name, hours, valueHour));
+                }
+
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS: ");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name + " - $" + emp.Payment());
+            }
+
 
         }
     }
